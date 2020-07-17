c1bc417e01775544e3656327902e44c5500e9088
Rahul <rahul.shinde@digitas.com>
2020-07-17 19:29:34 +0530

    Added Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
 import './App.css';
 
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<div>
+			<Header />
+			<div className='container'>
+				<Balance />
+				<IncomeExpense />
+				<TransactionList />
+				<AddTransaction />
+			</div>
+		</div>
+	);
 }
 
 export default App;
