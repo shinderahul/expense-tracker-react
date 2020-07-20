b3de1bf908ba91b14e09da21b06f5805cde93818
Rahul <rahul.shinde@digitas.com>
2020-07-20 18:59:02 +0530

    Delete transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -2,6 +2,13 @@ export default (state, action) => {
 	const { type, payload } = action;
 
 	switch (type) {
+		case 'DELETE_TRANSACTION':
+			return {
+				...state,
+				transactions: state.transactions.filter(
+					(transaction) => transaction.id !== payload
+				),
+			};
 		default:
 			return state;
 	}
